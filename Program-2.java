3369991106e54c05f964921bce182c03c3a19d34
Sahitya Naik <127645265+SahityaNaik@users.noreply.github.com>
2025-06-21 23:01:46 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -6,7 +6,7 @@
 
 import java.util.*;
 
-public class Problem2 {
+class Problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number a: ");
